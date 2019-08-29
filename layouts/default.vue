30e742249856eb268d27b30b0af98883b23a65c4
cryptodoctor <emmanuelneni@gmail.com>
2019-08-29 22:16:23 +0300

    aligning cursur animation to cursur image

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,35 +22,6 @@ export default {
 </script>
 
 <style lang="scss">
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-
 body {
   margin: 0;
   height: 100vh;
@@ -65,19 +36,14 @@ body {
   overflow-x: hidden;
 }
 
-* {
-  // cursor: url(~assets/img/circle-cursor.png), auto !important;
+a,
+button,
+.nuxt-link,
+.btn {
+  &:hover {
+    cursor: url("~assets/img/circle-cursorx2.png"), auto;
+  }
 }
-// *:hover {
-//   outline: none;
-
-//   cursor: -webkit-image-set(
-//         url(~assets/img/circle-cursor.png) 1x,
-//         url(~assets/img/circle-cursorx2.png) 2x
-//       )
-//       0 0,
-//     default !important;
-// }
 
 p {
   margin-top: 20px;
--- plugins/cursor.js
+++ plugins/cursor.js
@@ -2,7 +2,7 @@ window.onNuxtReady(() => {
 
   const cursor = document.querySelector('.cursor');
   document.addEventListener('mousemove', e => {
-    cursor.setAttribute("style", "top: " + (e.pageY - 5) + "px; left: " + (e.pageX - 5) + "px;")
+    cursor.setAttribute("style", "top: " + (e.pageY - 1) + "px; left: " + (e.pageX - 2) + "px;")
   })
   document.addEventListener('click', () => {
     cursor.classList.add("expand");
